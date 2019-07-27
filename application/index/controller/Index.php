1336eb96315388c108b671fa5fcd617a26e6eee1
iszmxw <mail@54zm.com>
2019-07-27 16:36:45 +0800

    配置数据库信息

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -13,7 +13,7 @@ class Index
 
     public function hello()
     {
-        $res = Db::table('emlog_tag')->where([])->limit(0, 10);
+        $res = Db::table('emlog_tag')->where([])->limit(0, 10)->select();
         dump($res);
     }
 }
