4604e9d3a1d8c73741de349a5f3aaa68931e384d
追梦小窝 <mail@54zm.com>
2019-07-29 23:14:36 +0800

    模型的创建

--- application/api/model/Base.php
+++ application/api/model/Base.php
@@ -30,7 +30,7 @@ class Base extends Model
         if (empty($field)) {
             $field = [];
         }
-        $res = self::where($where)->order($orderby, $sort)->get($field);
+        $res = self::where($where)->order($orderby, $sort)->get();
         if (!empty($res)) {
             return $res;
         } else {
