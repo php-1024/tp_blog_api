e052eb1f05a6380e5a26276f0abd316414807b64
追梦小窝 <mail@54zm.com>
2019-07-29 23:00:39 +0800

    模型的创建

---
+++ application/api/model/Base.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\api\model;
+
+use think\Model;
+use traits\model\SoftDelete;
+
+class Base extends Model
+{
+    use SoftDelete;
+    // 开启自动写入时间戳字段
+    protected $autoWriteTimestamp = true;
+
+    // 定义时间戳字段名
+    protected $createTime = 'created_at';
+    protected $updateTime = 'updated_at';
+    protected $deleteTime = 'deleted_at';
+
+
+    // 获取单字段数据
+    public static function getValue($where, $value)
+    {
+        return self::where($where)->value($value);
+    }
+
+    // 获取单组数据
+    public static function getOne($where = [], $field = [], $orderby = "id", $sort = 'DESC')
+    {
+        // 默认获取全部字段
+        if (empty($field)) {
+            $field = "*";
+        }
+        $res = self::where($where)->orderBy($orderby, $sort)->select($field);
+        if (!empty($res)) {
+            return $res;
+        } else {
+            return false;
+        }
+    }
+
+}
