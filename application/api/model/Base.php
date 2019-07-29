7539790b58d48c93b3abb855764c7309318a3eee
追梦小窝 <mail@54zm.com>
2019-07-29 23:36:01 +0800

    模型的创建

--- application/api/model/Base.php
+++ application/api/model/Base.php
@@ -11,6 +11,8 @@ class Base extends Model
     // 开启自动写入时间戳字段
     protected $autoWriteTimestamp = true;
 
+    protected $resultSetType = 'collection';
+
     // 定义时间戳字段名
     protected $createTime = 'created_at';
     protected $updateTime = 'updated_at';
