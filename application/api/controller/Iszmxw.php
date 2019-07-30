901a03317e40ab2b84af7c7246419428cd42e2fb
追梦小窝 <mail@54zm.com>
2019-07-30 20:29:48 +0800

    基础模型的添加

--- application/api/controller/Iszmxw.php
+++ application/api/controller/Iszmxw.php
@@ -71,7 +71,7 @@ class Iszmxw
     {
         Db::startTrans();
         try {
-            Blog::selected_delete(['id' => 2]);
+            Blog::selected_delete(['id' => 1]);
             Db::commit();
         } catch (\Exception $e) {
             dump($e);
