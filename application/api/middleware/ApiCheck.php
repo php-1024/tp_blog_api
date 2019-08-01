8af3b987f751d14ef5bffd6ded2ca0c1405c1647
iszmxw <mail@54zm.com>
2019-08-01 18:14:22 +0800

    master

--- application/api/middleware/ApiCheck.php
+++ application/api/middleware/ApiCheck.php
@@ -5,13 +5,13 @@ namespace app\api\middleware;
 use think\Controller;
 use think\Request;
 
-class ApiCheck extends Controller
+class ApiCheck
 {
     //配置tp5的中间件
     public function run()
     {
         $request = Request::instance();
         $request->bind('test', '测试');
-        return json(['code' => 50000, 'message' => '对不起您没有权限啊啊啊']);
+        return json(['code' => 50000, 'message' => '对不起您没有权限，对不起']);
     }
 }
