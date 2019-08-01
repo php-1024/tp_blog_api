fecf42f1eb0714239efb838fb5fed1b303ff2feb
iszmxw <mail@54zm.com>
2019-08-01 16:50:18 +0800

    master

--- application/api/behavior/UserCheck.php
+++ application/api/behavior/UserCheck.php
@@ -2,16 +2,18 @@
 
 namespace app\api\behavior;
 
+use think\Controller;
 use think\Request;
 
-class UserCheck
+class UserCheck extends Controller
 {
     //配置tp5的中间件
     public function run()
     {
         $request = Request::instance();
         $request->bind('test', '測試參數');
-        return ['code' => 50000, 'message' => '对不起您没有权限'];
+        $this->error('对不起您没有权限');
+//        return ['code' => 50000, 'message' => '对不起您没有权限'];
 
     }
 }
