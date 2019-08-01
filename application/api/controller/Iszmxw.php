64722879a5293636f204ba6da2ee3427ae8240c4
iszmxw <mail@54zm.com>
2019-08-01 16:42:48 +0800

    master

--- application/api/controller/Iszmxw.php
+++ application/api/controller/Iszmxw.php
@@ -18,7 +18,7 @@ class Iszmxw extends Controller
         $list = Blog::getList();
         $paginate = Blog::getPaginate([], [], 12, 'id', 'ASC');
         dump($request->param());
-        dump(request()->test);
+        dump($request->__get('test'));
         dump($request);
         if (empty($id)) {
             dump("缺少查询的id");
