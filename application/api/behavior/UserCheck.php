8feb4cd0c811cfe234b149cb1ad31925d802e62e
iszmxw <mail@54zm.com>
2019-08-01 15:06:23 +0800

    配置中间件环节

--- application/api/behavior/UserCheck.php
+++ application/api/behavior/UserCheck.php
@@ -5,10 +5,10 @@ namespace app\api\behavior;
 use think\Controller;
 use think\Request;
 
-class UserCheck extends Controller
+class UserCheck
 {
     //类里面引入jump;类 可以使用跳转的一些方法 还有 success 、error
-    public function run(Request $request)
+    public function run()
     {
         halt('测试HOOK');
     }
