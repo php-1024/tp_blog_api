77a87d937ec406ea32e42425208fb37e38ef7074
iszmxw <mail@54zm.com>
2019-08-01 15:27:08 +0800

    配置中间件环节

--- application/routes/api.php
+++ application/routes/api.php
@@ -13,12 +13,12 @@ use think\Route;
 
 Route::group('api', function () {
     // 追梦小窝专用测试路由
-    Route::group('iszmxw', ['before_behavior' => '\app\api\behavior\UserCheck'], function () {
+    Route::group('iszmxw', function () {
         Route::any('login', 'api/Iszmxw/login');
         Route::any('create', 'api/Iszmxw/create');
         Route::any('delete', 'api/Iszmxw/delete');
         Route::any('edit', 'api/Iszmxw/edit');
-    });
+    }, ['before_behavior' => '\app\api\behavior\UserCheck']);
 
     Route::group('user', function () {
         Route::any('login', 'api/Login/login');
