3afc004aed821aa5e6d34683458cdc2d4d4a2a99
iszmxw <mail@54zm.com>
2019-08-07 16:52:36 +0800

    master

---
+++ application/iszmxw/controller/ExportExcel.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace app\api\controller;
+
+use app\api\model\Blog;
+use app\api\model\Facility;
+use think\Db;
+use think\Request;
+use think\Controller;
+
+class ExportExcel extends Controller
+{
+
+    // 导出excel方法
+    public function export()
+    {
+        $list = Facility::where([])->select([
+            'id',
+            'machine_code',
+            'machine_name',
+            'address',
+            'lat',
+            'lng',
+            'tag_id',
+        ]);
+        dump($list);
+    }
+
+    public function login(Request $request)
+    {
+        $id = $request->get('id');
+        $title = Blog::getValue(['id' => $id], 'title');
+        $blog = Blog::getOne(['id' => $id]);
+        $list = Blog::getList();
+        $paginate = Blog::getPaginate([], [], 12, 'id', 'ASC');
+        dump($request->param());
+        dump($request->__get('test'));
+        dump($request);
+        if (empty($id)) {
+            dump("缺少查询的id");
+        } else {
+            dump($title);
+            dump($blog);
+        }
+        dump($paginate);
+    }
+
+    public function create(Request $request)
+    {
+        Db::startTrans();
+        try {
+            Blog::AddData([
+                'sort_id' => 1,
+                'title' => "我是标题",
+                'content' => "我是文章内容",
+                'excerpt' => "摘录",
+                'alias' => "别名"
+            ]);
+            Db::commit();
+        } catch (\Exception $e) {
+            dump($e);
+            Db::rollback();
+        }
+        return "添加数据成功";
+    }
+
+
+    public function edit(Request $request)
+    {
+        Db::startTrans();
+        try {
+            Blog::EditData([
+                'id' => 1
+            ], [
+                'title' => "哈哈成功搞定",
+                'content' => "修改了文章内容",
+                'excerpt' => "编辑摘录",
+                'alias' => "编辑别名"
+            ]);
+            Db::commit();
+        } catch (\Exception $e) {
+            dump($e);
+            Db::rollback();
+            return "修改数据失败";
+        }
+        return "修改数据成功";
+    }
+
+
+    public function delete(Request $request)
+    {
+        $id = $request->get('id');
+        Db::startTrans();
+        try {
+            $re = Blog::selected_delete(['id' => $id], true);
+            Db::commit();
+        } catch (\Exception $e) {
+            dump($e);
+            Db::rollback();
+            return "删除失败";
+        }
+        dump($re);
+    }
+}
---
+++ application/routes/iszmxw.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+use think\Route;
+
+Route::bind('iszmxw'); // 绑定到api模块下
+
+Route::group('iszmxw', function () {
+    // 追梦小窝专用测试路由
+    Route::any('export_excel', 'ExportExcel/export_excel');
+});
