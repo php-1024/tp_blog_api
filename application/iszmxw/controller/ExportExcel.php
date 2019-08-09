0e112f6a470be5651e82f22648e31cde84202f5a
iszmxw <mail@54zm.com>
2019-08-09 14:27:54 +0800

    master

--- application/iszmxw/controller/ExportExcel.php
+++ application/iszmxw/controller/ExportExcel.php
@@ -191,8 +191,10 @@ class ExportExcel extends Controller
             $info = ['province' => '', 'city' => '', 'area' => ''];
         } else {
             $province = Province::where(['province_id' => $re['province_id']])->field('name')->find()->toArray();
-            $city = City::where(['city_id' => $re['city_id']])->field('name')->find()->toArray();
-            $area = Area::where(['area_id' => $re['area_id']])->field('name')->find()->toArray();
+            $city = City::where(['city_id' => $re['city_id']])->field('name')->find();
+            $city = empty($city) ? array() : $re->toArray();
+            $area = Area::where(['area_id' => $re['area_id']])->field('name')->find();
+            $area = empty($area) ? array() : $re->toArray();
             $info = [
                 'province' => $province['name'],
                 'city' => $city['name'],
