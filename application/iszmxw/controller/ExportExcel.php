415d210a62513c865f7f656e2cf087ea6d77253b
iszmxw <mail@54zm.com>
2019-08-09 15:53:04 +0800

    master

--- application/iszmxw/controller/ExportExcel.php
+++ application/iszmxw/controller/ExportExcel.php
@@ -19,8 +19,6 @@ class ExportExcel extends Controller
     // 导出excel方法
     public function export_excel(Request $request)
     {
-        dump($request);
-        exit;
         $list = Facility::where([])
             ->field([
                 'id',
