e9fd561284bfdbc2608f73860a8d670f00db31a9
追梦小窝 <mail@54zm.com>
2019-08-20 19:54:56 +0800

    master

--- application/iszmxw/controller/Tools.php
+++ application/iszmxw/controller/Tools.php
@@ -17,12 +17,17 @@ class Tools extends Controller
 {
     public function ip_address()
     {
+        $ip2region = new \Ip2Region();
         $ip_data = file_get_contents("ip.txt");
         $ip_data = explode("\n", $ip_data);
         $array = [];
         foreach ($ip_data as $key => $val) {
             $array[] = explode(" = ", $val);
         }
-        dump($array);
+
+        foreach ($array as $key => $val) {
+            $info = $ip2region->btreeSearch($val[0]);
+            echo "IP地址：{$val[0]} 尝试登陆了{$val[1]}次，检测到真实地址为{$info['region']}<br>";
+        }
     }
 }
