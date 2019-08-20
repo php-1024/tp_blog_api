4c61e9588916646b277619d8fd3d9baccf7dfb50
追梦小窝 <mail@54zm.com>
2019-08-20 22:44:50 +0800

    master

--- application/routes/iszmxw.php
+++ application/routes/iszmxw.php
@@ -16,6 +16,8 @@ Route::bind('iszmxw'); // 绑定到api模块下
 Route::group('iszmxw', function () {
     // 追梦小窝专用测试路由
     Route::any('export_excel', 'ExportExcel/export_excel');
+    // json转换为excel
+    Route::any('json_excel', 'ExportExcel/json_excel');
     // 查看IP地址
     Route::any('ip_address', 'Tools/ip_address');
 });
