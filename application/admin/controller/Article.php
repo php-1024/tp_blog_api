6256879cea518beee567bbf6c13aeebcc256740d
追梦小窝 <mail@54zm.com>
2019-09-28 21:27:35 +0800

    master

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -17,8 +17,8 @@ class Article extends Controller
     public function article_list(Request $request)
     {
         $article_list = Blog::getPaginate([], ['id', 'sort_id', 'title', 'views', 'created_at'], 10, 'id', 'DESC');
-        foreach ($article_list as $key => $val) {
-            $article_list[$key]['sore_name'] = Sort::getValue(['id' => $val['sore_id']], 'name');
+        foreach ($article_list['data'] as $key => $val) {
+            $article_list['data'][$key]['sort_name'] = Sort::getValue(['id' => $val['sort_id']], 'name');
         }
         return json(['code' => 200, 'message' => 'ok', 'data' => $article_list]);
     }
