6f010b5c3dfe0b57d0904d615e5e490f87759728
追梦小窝 <mail@54zm.com>
2019-10-16 21:27:27 +0800

    先下拉=>后上传

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -7,6 +7,7 @@ use app\admin\model\Sort;
 use think\Db;
 use think\Request;
 use think\Controller;
+use think\Validate;
 
 class Category extends Controller
 {
@@ -55,6 +56,13 @@ class Category extends Controller
     }
 
 
+    /**
+     * 编辑栏目
+     * @param Request $request
+     * @return \think\response\Json
+     * @author：iszmxw <mail@54zm.com>
+     * @time：2019/10/16 20:00
+     */
     public function category_edit(Request $request)
     {
         $data   = $request->param();
@@ -63,5 +71,16 @@ class Category extends Controller
             // 验证失败 输出错误信息
             return json(['code' => 500, 'message' => $result]);
         }
+        Db::startTrans();
+        try {
+            Sort::EditData(['id' => $data['id']], $data);
+            Db::commit();
+            return json(['code' => 200, 'message' => '编辑栏目成功！']);
+        } catch (\Exception $e) {
+            Db::rollback();
+            return json(['code' => 500, 'message' => '编辑栏目失败，请稍后再试！']);
+        }
+
     }
+
 }
