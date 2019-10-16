7d1009007ef06899f69798122d5a894c34804b6b
追梦小窝 <mail@54zm.com>
2019-10-16 19:18:44 +0800

    先下拉=>后上传

--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -20,18 +20,18 @@ class System extends Controller
      */
     public function config(Request $request)
     {
-        $admin_data = $request->__get('admin_data');
-        $site_title = Options::getOption('site_title');
+        $admin_data       = $request->__get('admin_data');
+        $site_title       = Options::getOption('site_title');
         $site_description = Options::getOption('site_description');
-        $site_url = Options::getOption('site_url');
-        $icp = Options::getOption('icp');
-        $footer_info = Options::getOption('footer_info');
+        $site_url         = Options::getOption('site_url');
+        $icp              = Options::getOption('icp');
+        $footer_info      = Options::getOption('footer_info');
         return json(['code' => 200, 'message' => 'ok', 'data' => [
-            'site_title' => $site_title,
+            'site_title'       => $site_title,
             'site_description' => $site_description,
-            'site_url' => $site_url,
-            'icp' => $icp,
-            'footer_info' => $footer_info,
+            'site_url'         => $site_url,
+            'icp'              => $icp,
+            'footer_info'      => $footer_info,
         ]]);
     }
 
